dc63a77a92a462d78e417d81f1ab23effd320177
Thomas Allmer <thomas.allmer@ing.nl>
2017-08-18 20:02:08 +0000

    [BUGFIX] Release check also needs new json api

--- scripts/bower/release-check.sh
+++ scripts/bower/release-check.sh
@@ -7,9 +7,14 @@ if [ "$1" == "ycliCommands" ]; then
 	return;
 fi
 
+if [[ ! -f bower.json ]]; then
+	echo "[ERROR] There is no bower.json in your current directory $(pwd)";
+	return 1;
+fi
+
 currentDir=$(pwd);
-packageName=$(ycli util json ${currentDir}/package.json get name);
-currentVersion=$(ycli util json ${currentDir}/package.json get version);
+packageName=$(ycli util json get name --file ${currentDir}/bower.json);
+currentVersion=$(ycli util json get version --file ${currentDir}/bower.json);
 bowerVersion=$(bower info "$packageName" --verbose | grep -A1 'Available versions:' | tail -n 1 | sed -r 's/^.{4}//');
 
 if [[ "$currentVersion" == "$bowerVersion" ]]; then
