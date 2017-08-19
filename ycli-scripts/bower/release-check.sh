01aca22a05bcf77ea27affaded821e5af05f6fd5
Thomas Allmer <thomas.allmer@ing.nl>
2017-08-19 17:49:45 +0000

    [TASK] Better info messages for release-check

--- ycli-scripts/bower/release-check.sh
+++ ycli-scripts/bower/release-check.sh
@@ -18,9 +18,9 @@ currentVersion=$(ycli util json get version --file ${currentDir}/bower.json);
 bowerVersion=$(bower info "$packageName" --verbose | grep -A1 'Available versions:' | tail -n 1 | sed -r 's/^.{4}//');
 
 if [[ "$currentVersion" == "$bowerVersion" ]]; then
-	echo "✓ Bower has the same latest version $currentVersion";
+	echo "✓ Bower Repository has the same latest version $currentVersion";
 else
-	echo "✗ Not available with Bower";
+	echo "✗ Bower Repository has a different latest version";
 	echo "Version in package.json: $currentVersion";
-	echo "Version on Bower:  $bowerVersion";
+	echo "Version on Bower Repository: $bowerVersion";
 fi
