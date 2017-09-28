817db42e132d7f88055b4a6aff1aeb97999c0424
Thomas Allmer <thomas.allmer@ing.nl>
2017-09-28 14:30:00 +0000

    [FEATURE] set-versions now help and reset-patch

--- ycli-scripts/bower/set-versions/set-versions.js
+++ ycli-scripts/bower/set-versions/set-versions.js
@@ -4,30 +4,54 @@ const semver = require('semver');
 
 const filePath = process.cwd() + '/bower.json';
 const updateJsonFilePath = process.argv[2];
-const option = process.argv[3];
-
-options = {
-	mode: 'caret'
-};
-if (option === '--help') {
-	console.log('Options:');
-	console.log('--fixed');
-	console.log('--tilde');
-	console.log('--as-is');
-	return;
-}
-if (option === '--fixed') {
-	options.mode = 'fixed';
-}
-if (option === '--tilde') {
-	options.mode = 'tilde';
-}
-if (option === '--as-is') {
-	options.mode = 'as-is';
-}
-
 let bowerJson = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 let updateJson = JSON.parse(fs.readFileSync(updateJsonFilePath, 'utf8'));
+
+options = {
+	mode: 'caret',
+	resetPatch: false
+};
+
+let params = process.argv;
+for (let key of params.keys()) {
+	let param = params[key];
+	switch (param) {
+		case '-f':
+		case '--fixed':
+			options.mode = 'fixed';
+			break;
+		case '-t':
+		case '--tilde':
+			options.mode = 'tilde';
+			break;
+		case '-a':
+		case '--as-is':
+			options.mode = 'as-is';
+			break;
+		case '-r':
+		case '--reset-patch':
+			options.resetPatch = true;
+			break;
+		case '--help':
+			console.log('');
+			console.log('Bower Set Versions');
+			console.log('');
+			console.log('Set versions as defined in a json file.');
+			console.log('');
+			console.log('Options:');
+			console.log('  -f --fixed: set hard version e.g. 1.2.3');
+			console.log('  -t --tilde: set tilde version e.g. ~1.2.3');
+			console.log('  -r --reset-patch: set patch to 0 e.g. 1.2.3 becomes 1.2.0');
+			console.log('  -a --as-is: Include the versions of the dependencies');
+			console.log('');
+			console.log('Examples:');
+			console.log('  set-versions path/to/wanted-versions.json');
+			console.log('    => set versions according to the content of wanted-versions.json');
+			console.log('');
+			return;
+			break;
+	}
+}
 
 let didSomething = false;
 
@@ -40,6 +64,9 @@ function prepareVersion(version) {
 	let major = parseInt(parts[0].match(/\d/)) || 0;
 	let minor = parseInt(parts[1].match(/\d/)) || 0;
 	let patch = parseInt(parts[2].match(/\d/)) || 0;
+	if (options.resetPatch) {
+		patch = 0;
+	}
 	let semVersion = `${major}.${minor}.${patch}`;
 	switch(options.mode) {
 		case 'caret':
